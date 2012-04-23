d4f7d9d6ed4630e35e56d42b5d8e702cda3d8b0f
Herr_Alucard <adrain.f.tepes@gmail.com>
2012-04-23 18:02:40 +0800

    modified:   include/utils.h

--- tiger/include/utils.h
+++ tiger/include/utils.h
@@ -32,3 +32,7 @@
 /* Memory */
 #define MEM_ALLOC(type) (type*)malloc((size_t)sizeof(type))
 #define MEM_CALLOC(n,s) (char*)calloc((n),(s))
+#define MEM_ZERO(p,type,n) memset((p),0,sizeof(type)*(n))
+#define MEM_MOVE(to,from,type,n) memmove((to),(from),sizeof(type)*(n))
+#define MEM_COPY(to,from,type,n) memcpy((to),(from),sizeof(type)*(n))
+#define MEM_CMP(p1,p2,type,n) memcmp((p1),(p2),sizeof(type)*(n))
